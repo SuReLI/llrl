477c876bcf4a43fbde39b03fd291d7730462cc3d
erwanlecarpentier <erwan.lc2@gmail.com>
2019-02-05 20:41:09 -0500

    spaces repo created

---
+++ llrl/spaces/discrete.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class Discrete:
+    def __init__(self, n):
+        self.n = n
+
+    def __repr__(self):
+        return "Discrete(%d)" % self.n
+
+    def __eq__(self, other):
+        return self.n == other.n
+        
+    def sample(self):
+        return np.random.randint(self.n)
+        
+    def shape(self):
+        return (self.n,)
