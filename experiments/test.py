4ba3ac9acd6d2849905a553cdd4f472ebbd0773c
erwanlecarpentier <erwan.lc2@gmail.com>
2019-02-13 14:46:17 -0500

    Test script updated with new metric test (best-single-state-match metric)

--- experiments/test.py
+++ experiments/test.py
@@ -1,5 +1,3 @@
-from numpy.core._multiarray_umath import ndarray
-
 from llrl.envs.gridworld import GridWorld
 from llrl.algorithms.dynamic_programming import dynamic_programming
 from llrl.envs.handler import Handler
@@ -16,44 +14,68 @@ def print_state_distances(d):
     print('max       :', diag.max())
 
 
-# Parameters
-h = Handler()
-m1 = GridWorld(map_name='maze1', is_slippery=False)
-m2 = GridWorld(map_name='maze2', is_slippery=False)
-m3 = GridWorld(map_name='maze3', is_slippery=False)
-gamma = 0.9
+def wasserstein_mdp_distance_test():
+    # Parameters
+    h = Handler()
+    m1 = GridWorld(map_name='maze1', is_slippery=False)
+    m2 = GridWorld(map_name='maze2', is_slippery=False)
+    m3 = GridWorld(map_name='maze3', is_slippery=False)
+    gamma = 0.9
+
+    # Compute
+    v1 = dynamic_programming(m1, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
+    v2 = dynamic_programming(m2, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
+
+    state_distances = h.bi_simulation_distance(m1, m2)
+    distance_m1_m2 = h.mdp_distance(m1, m2, state_distances)
+    lipschitz_constant = (float(m1.nS) / (h.cr * (1.0 - gamma)))
+    gap = lipschitz_constant * distance_m1_m2
+
+    upper_bounds = np.array(v1, dtype=float)
+    for i in range(len(upper_bounds)):
+        upper_bounds[i] += gap
+
+    # Display
+    print('Display environments')
+    m1.render()
+    m2.render()
+    print('Value function 1        :')
+    m1.display_to_m(v1)
+    print('Value function 2        :')
+    m1.display_to_m(v2)
+    print('Difference between both :')
+    m1.display_to_m(abs(v1 - v2))
+    print()
+    print('Distance between states :\n', state_distances)
+    print('Distance between MDPs d = ', distance_m1_m2)
+    print('Lipschitz constant    K = ', lipschitz_constant)
+    print('Gap               K * d = ', gap)
+    print('Upper-bounds for MDP 2  :')
+    m1.display_to_m(upper_bounds)
 
 
-# Compute
-v1 = dynamic_programming(m1, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
-v2 = dynamic_programming(m2, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
+def best_match_mdp_distance_test():
+    # Parameters
+    h = Handler()
+    m1 = GridWorld(map_name='maze1', is_slippery=False)
+    m2 = GridWorld(map_name='maze2', is_slippery=False)
+    gamma = 0.9
 
+    # Compute
+    v1 = dynamic_programming(m1, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
+    v2 = dynamic_programming(m2, gamma=gamma, threshold=1e-10, iter_max=1000, verbose=False)
 
-state_distances = h.bi_simulation_distance(m1, m2)
-distance_m1_m2 = h.mdp_distance(m1, m2, state_distances)
-lipschitz_constant = (float(m1.nS) / (h.cr * (1.0 - gamma)))
-gap = lipschitz_constant * distance_m1_m2
+    # Display
+    print('Display environments')
+    m1.render()
+    m2.render()
+    print('Value function 1        :')
+    m1.display_to_m(v1)
+    print('Value function 2        :')
+    m1.display_to_m(v2)
+    print('Difference between both :')
+    m1.display_to_m(abs(v1 - v2))
 
-upper_bounds = np.array(v1, dtype=float)
-for i in range(len(upper_bounds)):
-    upper_bounds[i] += gap
-
-
-# Display
-print('Display environments')
-m1.render()
-m2.render()
-print('Value function 1        :')
-m1.display_to_m(v1)
-print('Value function 2        :')
-m1.display_to_m(v2)
-print('Difference between both :')
-m1.display_to_m(abs(v1 - v2))
-print()
-
-print('Distance between states :\n', state_distances)
-print('Distance between MDPs d = ', distance_m1_m2)
-print('Lipschitz constant    K = ', lipschitz_constant)
-print('Gap               K * d = ', gap)
-print('Upper-bounds for MDP 2  :')
-m1.display_to_m(upper_bounds)
+if __name__ == "__main__":
+    # wasserstein_mdp_distance_test()
+    best_match_mdp_distance_test()
