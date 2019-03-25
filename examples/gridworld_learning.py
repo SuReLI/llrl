944216e26ef442b1e76642e62ba923e80f54b5ff
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-24 20:12:37 -0400

    RMaxVI tested + clean-up

--- examples/gridworld_learning.py
+++ examples/gridworld_learning.py
@@ -2,6 +2,7 @@ from simple_rl.agents import QLearningAgent, RandomAgent
 from simple_rl.tasks import GridWorldMDP
 from simple_rl.run_experiments import run_agents_on_mdp
 from llrl.agents.rmax import RMax
+from llrl.agents.rmax_vi import RMaxVI
 
 
 def main():
@@ -11,10 +12,11 @@ def main():
     # Setup Agents.
     ql_agent = QLearningAgent(actions=mdp.get_actions())
     rand_agent = RandomAgent(actions=mdp.get_actions())
-    rmax_agent = RMax(actions=mdp.get_actions(), gamma=.9, horizon=3, count_threshold=1)
+    rmax_vi_agent = RMaxVI(actions=mdp.get_actions(), gamma=.9, horizon=10, count_threshold=1)
+    rmax_agent = RMax(actions=mdp.get_actions(), gamma=.9, horizon=10, count_threshold=1)
 
     # Run experiment and make plot.
-    run_agents_on_mdp([rmax_agent, ql_agent, rand_agent], mdp, instances=10,
+    run_agents_on_mdp([rmax_vi_agent, rmax_agent, ql_agent, rand_agent], mdp, instances=5,
                       episodes=100, steps=20, reset_at_terminal=True, verbose=False)
 
 
--- llrl/agents/rmax_vi.py
+++ llrl/agents/rmax_vi.py
@@ -1,6 +1,6 @@
 """
 Implementation of an R-Max agent [Brafman and Tennenholtz 2003]
-Use Value Iteration (VI) to compute the R-Max upper-bound.
+Use Value Iteration to compute the R-Max upper-bound.
 """
 
 import random
@@ -15,15 +15,13 @@ from simple_rl.agents.AgentClass import Agent
 class RMaxVI(Agent):
     """
     Implementation of an R-Max agent [Brafman and Tennenholtz 2003]
-    Use Value Iteration (VI) to compute the R-Max upper-bound.
+    Use Value Iteration to compute the R-Max upper-bound.
     """
 
-    def __init__(self, actions, gamma=0.9, horizon=3, count_threshold=1, name="RMaxVI-h"):
-        name = name + str(horizon) if name[-2:] == "-h" else name
+    def __init__(self, actions, gamma=0.9, count_threshold=1, name="RMaxVI"):
         Agent.__init__(self, name=name, actions=actions, gamma=gamma)
         self.nA = len(self.actions)
         self.r_max = 1.0
-        self.horizon = horizon
         self.count_threshold = count_threshold
 
         self.U, self.R, self.T, self.counter = self.empty_memory_structure()
@@ -34,9 +32,6 @@ class RMaxVI(Agent):
         """
         Reset the attributes to initial state.
         Save the previous model.
-
-        TODO check whether reset is only applied when sampling new task in lifelong setting -> not between instances
-
         :return: None
         """
         self.U, self.R, self.T, self.counter = self.empty_memory_structure()
@@ -76,7 +71,6 @@ class RMaxVI(Agent):
         print('Action space           :', self.actions)
         print('Number of actions      :', self.nA)
         print('Gamma                  :', self.gamma)
-        print('Horizon                :', self.horizon)
         print('Count threshold        :', self.count_threshold)
 
     def is_known(self, s, a):
@@ -95,7 +89,6 @@ class RMaxVI(Agent):
         """
         self.update(self.prev_s, self.prev_a, r, s)
 
-        # _, a = self._compute_max_q_value_action_pair(s)  # TODO remove
         a = self.greedy_action(s)
 
         self.prev_a = a
@@ -107,7 +100,6 @@ class RMaxVI(Agent):
         """
         Updates transition and reward dictionaries with the input transition
         tuple if the corresponding state-action pair is not known enough.
-
         :param s: int state
         :param a: int action
         :param r: float reward
@@ -151,111 +143,9 @@ class RMaxVI(Agent):
                     n_s_a = float(self.counter[s][a])
                     r_s_a = sum(self.R[s][a]) / n_s_a
 
-                    # METHOD 1
                     s_p_dict = self.T[s][a]
-                    s_p_weights = defaultdict(float)
-                    denominator = float(sum(s_p_dict.values()))
-                    for s_p in s_p_dict:
-                        s_p_weights[s_p] = s_p_dict[s_p] / denominator
-                    weighted_next_upper_bound = sum(
-                        [self.U[s_p][self.greedy_action(s_p)] * s_p_weights[s_p] for s_p in s_p_dict]
-                    )
-                    # print(weighted_next_upper_bound)
-
-                     # METHOD 2 TODO compare
-                    s_p_dict = self.T[s][a]
-                    denominator = float(sum(s_p_dict.values()))
                     weighted_next_upper_bound = 0.
                     for s_p in s_p_dict:
-                        weighted_next_upper_bound += self.U[s_p][self.greedy_action(s_p)] * s_p_dict[s_p] / denominator
-                    # print(weighted_next_upper_bound)
-
-                    # TODO compare denominator and n_s_a
+                        weighted_next_upper_bound += self.U[s_p][self.greedy_action(s_p)] * s_p_dict[s_p] / n_s_a
 
                     self.U[s][a] = r_s_a + self.gamma * weighted_next_upper_bound
-    '''
-    def _compute_max_q_value_action_pair(self, s, horizon=None):
-        """
-        TODO remove
-        Compute the greedy action wrt the current Q-value function.
-
-        :param s: int state
-        :param horizon: int horizon
-        :return: return the tuple (q_star, a_star) with q_star the maximum Q-value and a_star the maximizing action.
-        """
-        if horizon is None:
-            horizon = self.horizon
-        a_star = random.choice(self.actions)
-        q_star = self.compute_q_value(s, a_star, horizon)
-        for a in self.actions:
-            q_s_a = self.compute_q_value(s, a, horizon)
-            if q_s_a > q_star:
-                q_star = q_s_a
-                a_star = a
-        return q_star, a_star
-        
-    def compute_q_value(self, s, a, horizon=None):
-        """
-        TODO remove
-        Compute the learned Q-value at (s, a).
-
-        :param s: int state
-        :param a: int action
-        :param horizon: int number of steps ahead
-        :return: return Q(s, a)
-        """
-        if horizon is None:
-            horizon = self.horizon
-
-        r_s_a = self._get_reward(s, a)
-        if horizon <= 0 or s.is_terminal():
-            return r_s_a
-        else:
-            expected_future_return = self.gamma * self._compute_expected_future_return(s, a, horizon)
-            q_val = r_s_a + expected_future_return
-            return q_val
-
-    def _compute_expected_future_return(self, s, a, horizon=None):
-        """
-        TODO remove
-        Compute the expected return 1 step ahead from (s, a)
-
-        :param s: int state
-        :param a: int action
-        :param horizon: int number of steps ahead
-        :return: return the expected return 1 step ahead from (s, a)
-        """
-        if self.is_known(s, a):
-            if horizon is None:
-                horizon = self.horizon
-
-            s_p_dictionary = self.T[s][a]
-
-            denominator = float(sum(s_p_dictionary.values()))
-            s_p_weights = defaultdict(float)
-            for s_p in s_p_dictionary.keys():
-                count = s_p_dictionary[s_p]
-                s_p_weights[s_p] = (count / denominator)
-
-            weighted_future_returns = [self._compute_max_q_value_action_pair(s_p, horizon - 1)[0] * s_p_weights[s_p]
-                                       for s_p in s_p_dictionary.keys()]
-            return sum(weighted_future_returns)
-        else:
-            return self.r_max / (1. - self.gamma)
-
-    def _get_reward(self, s, a):
-        """
-        TODO remove
-        Return the learned expected reward function at (s, a) if known.
-        Else return the used heuristic.
-
-        :param s: int state
-        :param a: int action
-        :return: return R(s, a)
-        """
-        if self.is_known(s, a):
-            collected_rewards = self.R[s][a]
-            return float(sum(collected_rewards)) / float(len(collected_rewards))
-        else:
-            return self.r_max
-    '''
