f1c094257aab38797be1659da01d281ccc0c87a2
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-27 15:41:13 -0400

    Metadata project increased

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='llrl',
       version='0.0.1',
+      description='Code for experimenting with Lipschitz Lifelong Reinforcement Learning',
+      author='Erwan Lecarpentier',
+      author_email='erwan.lecarpentier@isae-supaero.fr',
       install_requires=['numpy']
 )
