0d5d806017cdbdeb93c31117fdf29d55851341e3
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-27 18:38:22 -0400

    Bound use recorder updated + plotting + time to convergence tracker + pandas included to venv

--- experiments/bounds_comparison.py
+++ experiments/bounds_comparison.py
@@ -8,21 +8,49 @@ Setting:
 """
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rc
 
+from llrl.utils.utils import csv_write
 from llrl.envs.gridworld import GridWorld
 from llrl.agents.lrmax_constant_transitions_testing import LRMaxCTTesting
 from simple_rl.run_experiments import run_agents_on_mdp
 
 
 SAVE_PATH = "results/bounds_comparison_results.csv"
-PRIOR = np.linspace(0., 1., num=11)
+ENTRIES = ["delta_r", "ratio_rmax_bound_use", "ratio_lip_bound_use", "n_time_steps", "n_time_steps_cv"]
+# PRIOR = np.linspace(0., 1., num=21)
+# PRIOR = [1., .9, .8, .7, .6, .5, .3, .28, .26, .24, .22, .2, .1, .0]
+PRIOR = [.0, .25, .5, .75, 1.]
 
 
-def main():
+def plot_results():
+    df = pd.read_csv(SAVE_PATH)
+    prior = df.delta_r
+    perct = 100. * df.ratio_lip_bound_use
+
+    # LaTeX rendering
+    rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
+    plt.plot(prior, perct, '-o')
+    plt.xlim(1., 0.)  # decreasing upper-bound
+    plt.xlabel(r'Prior knowledge: known upper-bound on $\max_{s, a} = |R_s^a - \bar{R}_s^a|$')
+    plt.ylabel(r'\% use Lipschitz bound')
+    # plt.title('')
+    plt.grid(True, linestyle='--')
+    plt.show()
+
+
+def bounds_test():
     # MDP
-    size = 5
+    size = 2
     mdp1 = GridWorld(width=size, height=size, init_loc=(1, 1), goal_locs=[(size, size)], goal_reward=1.0)
     mdp2 = GridWorld(width=size, height=size, init_loc=(1, 1), goal_locs=[(size, size)], goal_reward=0.8)
+
+    csv_write(ENTRIES, SAVE_PATH, 'w')
 
     for prior in PRIOR:
         lrmaxct = LRMaxCTTesting(actions=mdp1.get_actions(), gamma=.9, count_threshold=1, delta_r=prior, path=SAVE_PATH)
@@ -33,6 +61,11 @@ def main():
 
         run_agents_on_mdp([lrmaxct], mdp2, instances=1, episodes=100, steps=30,
                           reset_at_terminal=True, verbose=False, open_plot=False)
+
+
+def main():
+    bounds_test()
+    plot_results()
 
 
 if __name__ == "__main__":
--- llrl/agents/lrmax_constant_transitions_testing.py
+++ llrl/agents/lrmax_constant_transitions_testing.py
@@ -20,9 +20,11 @@ class LRMaxCTTesting(LRMaxCT):
         self.n_rmax = 0  # nb of times the rmax bound is used (smaller)
         self.n_lip = 0  # nb of times the lip bound is used (smaller)
 
+        self.n_time_steps = 0  # nb of time steps
+        self.n_time_steps_cv = 0  # nb of time steps before convergence with high probability
+
         # Save into csv file
         self.path = path
-        csv_write(["delta_r", "ratio_rmax_bound_use", "ratio_lip_bound_use"], self.path, 'w')
 
     def reset(self):
         """
@@ -37,7 +39,11 @@ class LRMaxCTTesting(LRMaxCT):
             # Save ratio
             ratio_rmax_bound_use = self.n_rmax / n_bound_use
             ratio_lip_bound_use = self.n_lip / n_bound_use
-            csv_write([self.delta_r, ratio_rmax_bound_use, ratio_lip_bound_use], self.path, 'a')
+            csv_write(
+                [self.delta_r, ratio_rmax_bound_use, ratio_lip_bound_use, self.n_time_steps, self.n_time_steps_cv],
+                self.path,
+                'a'
+            )
 
             # Reset
             self.n_rmax = 0
@@ -62,3 +68,43 @@ class LRMaxCTTesting(LRMaxCT):
                     if u[s][a] < u_min[s][a]:
                         u_min[s][a] = u[s][a]
         return u_min
+
+    def act(self, s, r):
+        """
+        Acting method called online during learning.
+        :param s: int current state of the agent
+        :param r: float received reward for the previous transition
+        :return: return the greedy action wrt the current learned model.
+        """
+        self.update(self.prev_s, self.prev_a, r, s)
+
+        a = self.greedy_action(s, self.min_upper_bound(s))
+
+        self.prev_a = a
+        self.prev_s = s
+
+        self.n_time_steps += 1  # INCREASE COUNTER BY 1
+
+        return a
+
+    def update(self, s, a, r, s_p):
+        """
+        Updates transition and reward dictionaries with the input transition
+        tuple if the corresponding state-action pair is not known enough.
+        If a new state-action pair is known, update both the R-Max upper-bound
+        and the Lipschitz upper-bounds.
+        :param s: state
+        :param a: action
+        :param r: reward
+        :param s_p: next state
+        :return: None
+        """
+        if s is not None and a is not None:
+            if self.counter[s][a] < self.count_threshold:
+                self.counter[s][a] += 1
+                self.R[s][a] += [r]
+                self.T[s][a][s_p] += 1
+                if self.counter[s][a] == self.count_threshold:
+                    self.n_time_steps_cv = self.n_time_steps  # RECORD LAST TIME A PAIR WAS UPDATED
+                    self.update_rmax_upper_bound()
+                    self.update_lipschitz_upper_bounds()
