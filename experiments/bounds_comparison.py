8939eee718fab3a14ca668317e6546c5c34abade
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-28 12:57:05 -0400

    Implemented confidence interval computation in bounds experiment.

--- experiments/bounds_comparison.py
+++ experiments/bounds_comparison.py
@@ -7,80 +7,118 @@ Setting:
 - Plotting percentage of bound use vs amount of prior knowledge.
 """
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-from llrl.utils.utils import csv_write
+from llrl.utils.utils import csv_write, mean_confidence_interval
 from llrl.envs.gridworld import GridWorld
 from llrl.agents.lrmax_ct_exp import LRMaxCTExp
 from llrl.agents.rmax_vi_exp import RMaxVIExp
 from simple_rl.run_experiments import run_agents_on_mdp
 
 
-LRMAX_SAVE_PATH = "results/bounds_comparison_results_LRMAX.csv"
-RMAX_SAVE_PATH = "results/bounds_comparison_results_RMAX.csv"
-ENTRIES = ["delta_r", "ratio_rmax_bound_use", "ratio_lip_bound_use", "n_time_steps", "n_time_steps_cv"]
-# PRIOR = np.linspace(0., 1., num=21)
-PRIOR = [1., .9, .8, .7, .6, .5, .4, .35, .3, .25, .2, .1, .0]
-# PRIOR = [.0, .25, .5, .75, 1.]
+SAVE_PATH = 'results/bounds_comparison_results.csv'
+LRMAX_TMP_SAVE_PATH = 'results/tmp/bounds_comparison_results_LRMAX.csv'
+RMAX_TMP_SAVE_PATH = 'results/tmp/bounds_comparison_results_RMAX.csv'
+
+N_INSTANCES = 100
+
+# PRIOR = [1., .9, .8, .7, .6, .5, .4, .35, .3, .25, .2, .1, .0]
+PRIOR = [.0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]
 
 
 def plot_results():
     # 1. Results in terms of bound-use
-    df = pd.read_csv(LRMAX_SAVE_PATH)
-    prior = df.delta_r
-    perct = 100. * df.ratio_lip_bound_use
-
-    # 2. Results in terms of speed-up
-    df_rmax = pd.read_csv(RMAX_SAVE_PATH)
-    lrmax_times_to_cv = df.n_time_steps_cv
-    rmax_times_to_cv = df_rmax.n_time_steps_cv
-    speed_up = 100. * (rmax_times_to_cv - lrmax_times_to_cv) / rmax_times_to_cv
+    df = pd.read_csv(SAVE_PATH)
+    prior = df.prior
+    ratio = df.ratio_lip_bound_use_mean
+    ratio_up = df.ratio_lip_bound_use_upper
+    ratio_lo = df.ratio_lip_bound_use_lower
+    speed_up = df.speed_up_mean
+    speed_up_up = df.speed_up_upper
+    speed_up_lo = df.speed_up_lower
 
     # LaTeX rendering
     rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
 
-    plt.plot(prior, perct, '-o', label=r'\% use Lipschitz bound')
-    plt.plot(prior, speed_up, '-o', label=r'\% time-steps gained (speed-up)')
+    colors = ['steelblue', 'darkorange', 'green']
+    plt.plot(prior, ratio, '-o', color=colors[0], label=r'\% use Lipschitz bound')
+    plt.fill_between(prior, ratio_up, ratio_lo, color=colors[0], alpha=0.1)
+    plt.plot(prior, speed_up, '-o', color=colors[1], label=r'\% time-steps gained (speed-up)')
+    plt.fill_between(prior, speed_up_up, speed_up_lo, color=colors[1], alpha=0.1)
 
     plt.xlim(1., 0.)  # decreasing upper-bound
     plt.xlabel(r'Prior knowledge (known upper-bound on $\max_{s, a} = |R_s^a - \bar{R}_s^a|$)')
     plt.ylabel(r'\%')
-    plt.legend()
+    plt.legend(loc='best')
     # plt.title('')
     plt.grid(True, linestyle='--')
     plt.show()
 
 
-def bounds_test():
+def bounds_test(verbose=True):
     # MDP
-    size = 4
-    mdp1 = GridWorld(width=size, height=size, init_loc=(1, 1), goal_locs=[(size, size)], goal_reward=1.0)
-    mdp2 = GridWorld(width=size, height=size, init_loc=(1, 1), goal_locs=[(size, size)], goal_reward=0.8)
+    sz = 2
+    mdp1 = GridWorld(width=sz, height=sz, init_loc=(1, 1), goal_locs=[(sz, sz)], goal_reward=1.0)
+    mdp2 = GridWorld(width=sz, height=sz, init_loc=(1, 1), goal_locs=[(sz, sz)], goal_reward=0.8)
 
-    csv_write(["delta_r", "ratio_rmax_bound_use", "ratio_lip_bound_use", "n_time_steps", "n_time_steps_cv"], LRMAX_SAVE_PATH, 'w')
-    csv_write(["n_time_steps", "n_time_steps_cv"], RMAX_SAVE_PATH, 'w')
+    results = []
 
-    for prior in PRIOR:
-        lrmaxct = LRMaxCTTesting(actions=mdp1.get_actions(), gamma=.9, count_threshold=1, delta_r=prior, path=LRMAX_SAVE_PATH)
-        rmaxvi = RMaxVITesting(actions=mdp1.get_actions(), gamma=.9, count_threshold=1, path=RMAX_SAVE_PATH)
+    for _ in range(N_INSTANCES):
+        csv_write(['prior', 'ratio_rmax_bound_use', 'ratio_lip_bound_use', 'lrmax_n_time_steps', 'lrmax_n_time_steps_cv'], LRMAX_TMP_SAVE_PATH, 'w')
+        csv_write(['rmax_n_time_steps', 'rmax_n_time_steps_cv'], RMAX_TMP_SAVE_PATH, 'w')
 
-        # Run twice
-        run_agents_on_mdp([lrmaxct], mdp1, instances=1, episodes=100, steps=30,
-                          reset_at_terminal=True, verbose=False, open_plot=False)
+        for prior in PRIOR:
+            lrmaxct = LRMaxCTExp(actions=mdp1.get_actions(), gamma=.9, count_threshold=1, delta_r=prior, path=LRMAX_TMP_SAVE_PATH)
+            rmaxvi = RMaxVIExp(actions=mdp1.get_actions(), gamma=.9, count_threshold=1, path=RMAX_TMP_SAVE_PATH)
 
-        run_agents_on_mdp([lrmaxct, rmaxvi], mdp2, instances=1, episodes=100, steps=30,
-                          reset_at_terminal=True, verbose=False, open_plot=False)
+            # Run twice
+            run_agents_on_mdp([lrmaxct], mdp1, instances=1, episodes=100, steps=30,
+                              reset_at_terminal=True, verbose=False, open_plot=False)
+
+            run_agents_on_mdp([lrmaxct, rmaxvi], mdp2, instances=1, episodes=100, steps=30,
+                              reset_at_terminal=True, verbose=False, open_plot=False)
+
+        # Retrieve data
+        df_lrmax = pd.read_csv(LRMAX_TMP_SAVE_PATH)
+        df_rmax = pd.read_csv(RMAX_TMP_SAVE_PATH)
+        df = pd.concat([df_lrmax, df_rmax], axis=1, sort=False)
+        result = []
+        for index, row in df.iterrows():
+            prior = row['prior']
+            ratio_lip_bound_use = 100. * row['ratio_lip_bound_use']
+            speed_up = 100. * (row['rmax_n_time_steps_cv'] - row['lrmax_n_time_steps_cv']) / row['rmax_n_time_steps_cv']
+            result.append([prior, ratio_lip_bound_use, speed_up])
+        results.append(result)
+
+    # Gather results
+    csv_write(
+        ['prior',
+         'ratio_lip_bound_use_mean',
+         'ratio_lip_bound_use_upper',
+         'ratio_lip_bound_use_lower',
+         'speed_up_mean',
+         'speed_up_upper',
+         'speed_up_lower'],
+        SAVE_PATH, 'w'
+    )
+    for i in range(len(PRIOR)):
+        rlbu = []
+        su = []
+        for result in results:
+            rlbu.append(result[i][1])
+            su.append(result[i][2])
+        rlbu_mci = mean_confidence_interval(rlbu)
+        su_mci = mean_confidence_interval(su)
+        csv_write([PRIOR[i]] + list(rlbu_mci) + list(su_mci), SAVE_PATH, 'a')
+    if verbose:
+        for result in results:
+            print(result)
 
 
-def main():
+if __name__ == '__main__':
     bounds_test()
     plot_results()
-
-
-if __name__ == "__main__":
-    main()
--- llrl/utils/utils.py
+++ llrl/utils/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import scipy.stats
 from math import isclose
 
 
@@ -14,6 +15,20 @@ def csv_write(row, path, mode):
     with open(path, mode) as csvfile:
         w = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         w.writerow(row)
+
+
+def mean_confidence_interval(data, confidence=0.95):
+    """
+    Compute the mean and confidence interval of the the input data array-like.
+    :param data: (array-like)
+    :param confidence: probability of the mean to lie in the interval
+    :return: (tuple) mean, interval upper-endpoint, interval lower-endpoint
+    """
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
 
 
 def close(a, b, r=13):
