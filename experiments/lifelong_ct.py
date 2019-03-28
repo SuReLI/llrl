930431a7b27fd46c4610182011f67fa25f68476c
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-28 16:56:25 -0400

    clean-up

--- experiments/lifelong_ct.py
+++ experiments/lifelong_ct.py
@@ -3,10 +3,12 @@ Lifelong RL experiment in constant transition function setting
 """
 
 from llrl.agents.lrmax_ct import LRMaxCT
+from simple_rl.run_experiments import run_agents_lifelong
+from simple_rl.utils import make_mdp
 
 
 def experiment():
-    print('TODO')
+    mdp_distribution = make_mdp.make_mdp_distr(mdp_class="four_room")
 
 
 if __name__ == '__main__':
