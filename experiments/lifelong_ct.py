abb0a61a47dfa5a113026acaf512cd7c15a448bd
erwanlecarpentier <erwan.lc2@gmail.com>
2019-03-28 17:27:11 -0400

    Lifelong-CTGridWorld experiment implemented (single thread, single core)

--- experiments/lifelong_ct.py
+++ experiments/lifelong_ct.py
@@ -2,13 +2,26 @@
 Lifelong RL experiment in constant transition function setting
 """
 
-from llrl.agents.lrmax_ct import LRMaxCT
+# from llrl.agents.lrmax_ct import LRMaxCT
+from llrl.agents.rmax_vi import RMaxVI
+from llrl.utils.env_handler import make_env_distribution
 from simple_rl.run_experiments import run_agents_lifelong
-from simple_rl.utils import make_mdp
+
+
+GAMMA = .9
+WIDTH = 4
+HEIGHT = 4
 
 
 def experiment():
-    mdp_distribution = make_mdp.make_mdp_distr(mdp_class="four_room")
+    # Create environments distribution
+    env_distribution = make_env_distribution(env_class='grid-world', n_env=10, gamma=GAMMA, w=WIDTH, h=HEIGHT)
+
+    rmax = RMaxVI(actions=env_distribution.get_actions(), gamma=GAMMA, count_threshold=1)
+
+    run_agents_lifelong(
+        [rmax], env_distribution, samples=20, episodes=50, steps=100, reset_at_terminal=True, open_plot=True
+    )
 
 
 if __name__ == '__main__':
--- llrl/utils/env_handler.py
+++ llrl/utils/env_handler.py
@@ -8,16 +8,16 @@ from simple_rl.mdp import MDPDistribution
 from llrl.envs.gridworld import GridWorld
 
 
-def make_env_distribution(env_class='grid-world', n_env=10, gamma=.9, horizon=0, w=5, h=5, verbose=True):
+def make_env_distribution(env_class='grid-world', n_env=10, gamma=.9, w=5, h=5, horizon=0, verbose=True):
     """
     Create a distribution over environments.
     This function is specialized to the included environments.
     :param env_class: (str) name of the environment class
     :param n_env: (int) number of environments in the distribution
     :param gamma: (float) discount factor
-    :param horizon: (int)
     :param w: (int) width for grid-world
     :param h: (int) height for grid-world
+    :param horizon: (int)
     :param verbose: (bool) print info if True
     :return: (MDPDistribution)
     """
@@ -28,7 +28,7 @@ def make_env_distribution(env_class='grid-world', n_env=10, gamma=.9, horizon=0,
     env_dist_dict = {}
 
     for _ in range(n_env):
-        sampled_reward = np.random.uniform()
+        sampled_reward = np.random.uniform(.5, .7)
         new_env = GridWorld(
             width=w, height=h, init_loc=(1, 1), goal_locs=[(w, h)],
             gamma=gamma, slip_prob=0.0, goal_reward=sampled_reward, name="grid-world"
