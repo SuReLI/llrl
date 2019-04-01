4a20816b6716c607b405d0229dcaa3889142051e
erwanlecarpentier <erwan.lc2@gmail.com>
2019-04-01 14:47:04 -0400

    Multi-core processing implemented

--- examples/benchmark_example.py
+++ examples/benchmark_example.py
@@ -1,28 +1,25 @@
 """
-Simple example for use of the llrl.utils.benchmark functions.
+Simple example for multi-processing use.
 """
 
 import time
 
 from llrl.utils.save import csv_write
-from llrl.utils.benchmark import multi
+from llrl.utils.benchmark import multi_cores
 
 
-def my_atomic_function(core_number, thread_number):
+def my_atomic_function(core_number):
     """
     Atomic function to be executed by each core / thread.
     :param core_number: (int) core number identifier
-    :param thread_number: (int) thread number identifier
     :return: None
     """
-    path = 'tmp/benchmark-example-' + str(core_number) + '-' + str(thread_number) + '.csv'
-    for i in range(100):
+    path = 'tmp/benchmark-example-' + str(core_number) + '.csv'
+    for i in range(3):
         time.sleep(1)
         csv_write([i], path, 'w')
 
 
 if __name__ == "__main__":
     n_cores = 4
-    n_threads = 1
-
-    multi(my_atomic_function, n_cores, n_threads)
+    multi_cores(my_atomic_function, n_cores)
--- llrl/utils/benchmark.py
+++ llrl/utils/benchmark.py
@@ -3,20 +3,29 @@ Benchmark functions for multi-core / multi-threading processes
 """
 
 import multiprocessing
+import time
 
 
-def multi(atomic_function, n_cores, n_threads, verbose=True):
+def multi_cores(atomic_function, n_cores, core_number_as_arg=True, verbose=True):
     """
     Execute the input function on the given number of cores / threads.
     :param atomic_function: function to be executed taking only two arguments: (core_number, thread_number)
     :param n_cores: (int) number of cores
-    :param n_threads: (int) number of threads
+    :param core_number_as_arg: (bool)
+    :param verbose: (bool)
     :return: None
     """
+    if verbose:
+        print('Executing', atomic_function, '( number of cores:', n_cores, ')')
+
+    start = time.clock()
+
     jobs = []
     for i in range(n_cores):
-        j = 0  # TODO thread number
-        process = multiprocessing.Process(target=atomic_function, args=(i + 1, j + 1))
+        if core_number_as_arg:
+            process = multiprocessing.Process(target=atomic_function, args=(i + 1,))
+        else:
+            process = multiprocessing.Process(target=atomic_function)
         jobs.append(process)
 
     for job in jobs:
@@ -25,5 +34,8 @@ def multi(atomic_function, n_cores, n_threads, verbose=True):
     for job in jobs:
         job.join()
 
+    end = time.clock()
+
     if verbose:
-        print('Successfully executed', atomic_function, '( n_cores:', n_cores, 'n_threads:0', n_threads, ')')
+        print('Successfully executed', atomic_function, '( number of cores:', n_cores, ')')
+        print('Execution time:', str(round(end - start, 3)), '(s)')
