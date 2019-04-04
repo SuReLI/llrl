abc9d2fb0fbe4d7c9d71f6769a3a762380ad0e17
erwanlecarpentier <erwan.lc2@gmail.com>
2019-04-04 18:15:09 -0400

    Model approximator for approximate value iteration implemented

--- llrl/algorithms/value_iteration.py
+++ llrl/algorithms/value_iteration.py
@@ -1,19 +1,28 @@
-"""
-Value iteration algorithm
-
-Required features from the environment:
-env.nS
-env.nA
-env.expected_reward(s, a)
-env.transition_probability_distribution(s, a)
-"""
-
 import numpy as np
 
 
-def value_iteration(env, gamma=0.9, threshold=0.01, iter_max=1000, verbose=False):
-    nS = env.nS
-    nA = env.nA
+def value_iteration(env, gamma=0.9, threshold=0.01, iter_max=1000, verbose=True):
+    """
+    Exact Value iteration algorithm.
+
+    Required features from the environment:
+    env.number_of_states()
+    env.number_of_actions()
+    env.expected_reward(s, a)
+    env.transition_probability_distribution(s, a)
+
+    Expect the states of the environment to be integers.
+
+    :param env: environment object
+    :param gamma: discount factor
+    :param threshold: threshold for convergence (algorithms stops when the infinite
+    norm between subsequent value functions is below @threshold)
+    :param iter_max: upper bound on the maximum number of iterations
+    :param verbose:
+    :return: value function as a vector
+    """
+    nS = env.number_of_states()
+    nA = env.number_of_actions()
     value_function = np.zeros(shape=nS, dtype=float)
     convergence, niter = False, 0
     for i in range(iter_max):
@@ -38,3 +47,46 @@ def value_iteration(env, gamma=0.9, threshold=0.01, iter_max=1000, verbose=False
             print('Maximum number of iteration (', iter_max, ') reached before convergence.')
             print('Tolerance was', threshold)
     return value_function
+
+
+def approximate_model(env, epsilon=.1, delta=.05):
+    """
+    Compute an approximate model for the input environment.
+
+    :param env: see approximate_value_iteration function
+    :param epsilon: precision of the model (1-norm)
+    :param delta: the model is epsilon-accurate with probability 1 - delta
+    :return: tuple(dictionary, dictionary) reward_model, transition_model
+    """
+    states = env.states()
+    actions = env.actions()
+    m_r = np.log(2. / delta) / (2. * epsilon**2)
+    m_t = 2. * (np.log(2**(len(states)) - 2.) - np.log(delta)) / (epsilon**2)
+    n_iter = int(max(m_r, m_t))
+
+    print(m_r)
+    print(m_t)
+    print(n_iter)
+    exit()
+
+    #for s in states:
+    #return reward_model, transition_model
+
+
+def approximate_value_iteration(env, gamma, epsilon, verbose=True):
+    """
+    Approximate Value iteration algorithm.
+
+    Required features from the environment:
+    env.states() (array-like containing each state of the environment)
+    env.actions() (array-like containing each action of the environment)
+    env.reward_func(s, a) (sample a r(s, a))
+    env.transition_func(s, a) (sample resulting state from application of a at s)
+
+    :param env: environment object
+    :param gamma: discount factor
+    :param epsilon:
+    :param verbose:
+    :return: the value function as a dictionary
+    """
+    reward_model, transition_model = approximate_model(env)
