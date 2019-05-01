f21a794c7370a3ae262f6dfe81d12bd125a56d83
erwanlecarpentier <erwan.lc2@gmail.com>
2019-05-01 13:15:39 +0200

    Negative transfer example created (TODO implement envt)

---
+++ examples/negative_transfer.py
@@ -0,0 +1,16 @@
+"""
+Negative transfer example using Song et al. [2016] transfer method.
+"""
+
+
+def weighted_transfer():
+    print('TODO')
+
+
+def state_transfer():
+    print('TODO')
+
+
+if __name__ == "__main__":
+    weighted_transfer()
+    state_transfer()
