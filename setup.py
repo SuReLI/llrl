31c67df220101375d18d4961a5b190df748d1653
erwanlecarpentier <erwan.lc2@gmail.com>
2019-05-14 13:45:20 +0200

    Clean-up

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name='llrl',
-      version='0.0.1',
-      description='Code for experimenting with Lipschitz Lifelong Reinforcement Learning',
-      author='Erwan Lecarpentier',
-      author_email='erwan.lecarpentier@isae-supaero.fr',
-      install_requires=['numpy']
+setup(
+	name='llrl',
+	version='0.0.1',
+	description='Code for experimenting with Lipschitz Lifelong Reinforcement Learning',
+	author='Erwan Lecarpentier',
+	author_email='erwan.lecarpentier@isae-supaero.fr',
+	install_requires=['numpy']
 )
