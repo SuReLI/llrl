6c04f4f4184dd2a574b3385f5e155992287eadae
erwanlecarpentier <erwan.lc2@gmail.com>
2019-09-01 19:22:56 +0200

    Results for deterministic tight regular and big sizes added

--- deterministic-tight-big.py
+++ deterministic-tight-big.py
@@ -54,7 +54,7 @@ def experiment():
 
     # Run
     run_agents_lifelong(agents_pool, env_distribution, n_instances=10, n_tasks=100, n_episodes=100, n_steps=100,
-                        reset_at_terminal=False, open_plot=True, plot_title=True,
+                        reset_at_terminal=False, open_plot=True, plot_title=True, do_run=False, do_plot=True,
                         parallel_run=True, n_processes=None)
 
 
