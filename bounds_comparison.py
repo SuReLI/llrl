3cd9b8afeef3124e2a90bf31ea14f0681fc10514
erwanlecarpentier <erwan.lc2@gmail.com>
2019-09-17 14:42:42 +0200

    New bounds_comparison experiment script (enable multithreading)

--- bounds_comparison.py
+++ bounds_comparison.py
@@ -7,32 +7,70 @@ Setting:
 - Plotting percentage of bound use vs amount of prior knowledge + speed-up.
 """
 
+import os
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import multiprocessing
 from matplotlib import rc
 from tqdm import trange
 
 from llrl.utils.env_handler import *
 from llrl.utils.utils import mean_confidence_interval
-from llrl.utils.save import csv_write
+from llrl.experiments import apply_async
 from llrl.envs.gridworld import GridWorld
 from llrl.agents.experimental.lrmax_bounds_use import ExpLRMax
 from llrl.agents.experimental.rmax_bounds_use import ExpRMax
 from llrl.experiments import run_agents_on_mdp
 
 
-def plot_bound_use(path):
-    # 1. Results in terms of bound-use
-    df = pd.read_csv(path)
-    prior = df.prior
-    ratio = df.ratio_lip_bound_use_mean
-    ratio_up = df.ratio_lip_bound_use_upper
-    ratio_lo = df.ratio_lip_bound_use_lower
-    speed_up = df.speed_up_mean
-    speed_up_up = df.speed_up_upper
-    speed_up_lo = df.speed_up_lower
+def plot_bound_use(lrmax_path, rmax_path, n_run, confidence=.9, open_plot=False):
+    lrmax_df = pd.read_csv(lrmax_path)
+    rmax_df = pd.read_csv(rmax_path)
 
+    x = []
+    rlbu_m, rlbu_lo, rlbu_up = [], [], []
+    su_m, su_lo, su_up = [], [], []
+
+    for i in range(n_run):
+        ldf = lrmax_df.loc[lrmax_df['run_number'] == i]
+        rdf = lrmax_df.loc[rmax_df['run_number'] == i]
+
+        prior = ldf.iloc[0]['prior']
+
+        _rlbu_m, _rlbu_lo, _rlbu_up = mean_confidence_interval(np.array(ldf.ratio_lip_bound_use), confidence=confidence)
+
+        lrmax_ntscv = np.array(ldf.n_time_steps_cv)
+        rmax_ntscv = np.array(rdf.n_time_steps_cv)
+        speed_up = 100. * (rmax_ntscv - lrmax_ntscv) / rmax_ntscv
+        _su_m, _su_lo, _su_up = mean_confidence_interval(speed_up, confidence=confidence)
+
+        x.append(prior)
+        rlbu_m.append(_rlbu_m)
+        rlbu_lo.append(_rlbu_lo)
+        rlbu_up.append(_rlbu_up)
+        su_m.append(_su_m)
+        su_lo.append(_su_lo)
+        su_up.append(_su_up)
+
+        exit()
+
+        my_plot_bound_use(
+            path='results/bounds_comparison/',
+            pdf_name='bounds_comparison',
+            prior=x,
+            ratio=rlbu_m,
+            ratio_up=rlbu_up,
+            ratio_lo=rlbu_lo,
+            speed_up=su_m,
+            speed_up_up=su_up,
+            speed_up_lo=su_lo,
+            open_plot=open_plot
+        )
+
+
+def my_plot_bound_use(path, pdf_name, prior, ratio, ratio_up, ratio_lo, speed_up, speed_up_up, speed_up_lo, open_plot):
     # LaTeX rendering
     rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
     plt.rc('text', usetex=True)
@@ -50,7 +88,19 @@ def plot_bound_use(path):
     plt.legend(loc='best')
     # plt.title('')
     plt.grid(True, linestyle='--')
-    plt.show()
+
+    # Save
+    plot_file_name = os.path.join(path, pdf_name + '.pdf')
+    plt.savefig(plot_file_name, format='pdf')
+
+    # Open
+    if open_plot:
+        open_prefix = 'gnome-' if sys.platform == 'linux' or sys.platform == 'linux2' else ''
+        os.system(open_prefix + 'open ' + plot_file_name)
+
+    # Clear and close
+    plt.cla()
+    plt.close()
 
 
 def sample_environments(env_class, gamma, w, h, sto):
@@ -70,7 +120,27 @@ def sample_environments(env_class, gamma, w, h, sto):
     return mdp1, mdp2
 
 
-def bounds_comparison_experiment(verbose=False, plot=True):
+def run_twice(instance_number, run_number, rmax, lrmax, prior, mdp1, mdp2, n_episodes, n_steps):
+    lrmax.prior = prior
+    lrmax.re_init()
+    rmax.re_init()
+
+    lrmax.instance_number = instance_number
+    lrmax.run_number = run_number
+    rmax.instance_number = instance_number
+    rmax.run_number = run_number
+
+    # Run twice
+    lrmax.write_data = False
+    run_agents_on_mdp([lrmax], mdp1, n_instances=1, n_episodes=n_episodes, n_steps=n_steps,
+                      reset_at_terminal=False, verbose=False)
+
+    lrmax.write_data = True
+    run_agents_on_mdp([lrmax, rmax], mdp2, n_instances=1, n_episodes=n_episodes, n_steps=n_steps,
+                      reset_at_terminal=False, verbose=False)
+
+
+def bounds_comparison_experiment(do_run=False, do_plot=True, multi_thread=True, n_threads=None, open_plot=False):
     # Parameters
     gamma = 0.9
     n_instances = 10
@@ -79,7 +149,9 @@ def bounds_comparison_experiment(verbose=False, plot=True):
     prior_min = 1.  # (1. + gamma) / (1. - gamma)
     prior_max = 0.
     priors = [round(p, 1) for p in np.linspace(start=prior_min, stop=prior_max, num=5)]
-    save_path = 'results/bounds_comparison/results.csv'
+    path = 'results/bounds_comparison/'
+    lrmax_path = path + 'lrmax-results.csv'
+    rmax_path = path + 'rmax-results.csv'
 
     # Environments
     w, h = 11, 11
@@ -96,70 +168,69 @@ def bounds_comparison_experiment(verbose=False, plot=True):
     epsilon_m = .01
     delta = .1
 
-    # Agents
-    lrmax = ExpLRMax(actions=actions, gamma=gamma, r_max=r_max, v_max=v_max, deduce_v_max=deduce_v_max,
-                     n_known=n_known, deduce_n_known=False, epsilon_q=epsilon_q, epsilon_m=epsilon_m,
-                     delta=delta, n_states=n_states, max_memory_size=None, prior=0.,
-                     estimate_distances_online=True, min_sampling_probability=.5, name="ExpLRMax")
-    rmax = ExpRMax(actions=actions, gamma=gamma, r_max=r_max, v_max=v_max, deduce_v_max=deduce_v_max,
-                   n_known=n_known, deduce_n_known=False, epsilon_q=epsilon_q, epsilon_m=epsilon_m, delta=delta,
-                   n_states=n_states, name="ExpRMax")
+    if do_run:
+        lrmax = ExpLRMax(actions=actions, gamma=gamma, r_max=r_max, v_max=v_max, deduce_v_max=deduce_v_max,
+                         n_known=n_known, deduce_n_known=False, epsilon_q=epsilon_q, epsilon_m=epsilon_m,
+                         delta=delta, n_states=n_states, max_memory_size=None, prior=0.,
+                         estimate_distances_online=True, min_sampling_probability=.5, name="ExpLRMax", path=lrmax_path)
+        rmax = ExpRMax(actions=actions, gamma=gamma, r_max=r_max, v_max=v_max, deduce_v_max=deduce_v_max,
+                       n_known=n_known, deduce_n_known=False, epsilon_q=epsilon_q, epsilon_m=epsilon_m, delta=delta,
+                       n_states=n_states, name="ExpRMax", path=rmax_path)
 
-    results = []
+        lrmax.write(init=True)
+        rmax.write(init=True)
 
-    for _ in trange(n_instances, desc='{:>10}'.format('instances')):
-        df_lrmax, df_rmax = None, None
-        for i in trange(len(priors), desc='{:>10}'.format('priors')):
-            lrmax.prior = priors[i]
-            lrmax.re_init()
-            rmax.re_init()
+        if multi_thread:
+            n_processes = multiprocessing.cpu_count() if n_threads is None else n_threads
+            print('Using', n_processes, 'threads.')
+            pool = multiprocessing.Pool(processes=n_processes)
 
-            if i > 0:
-                lrmax.data = df_lrmax
-                rmax.data = df_rmax
+            # Asynchronous execution
+            jobs = []
+            for i in range(n_instances):
+                for j in range(len(priors)):
+                    job = apply_async(
+                        pool, run_twice, (i, j, rmax, lrmax, priors[j], mdp1, mdp2, n_episodes, n_steps)
+                    )
+                    jobs.append(job)
 
-            # Run twice
-            lrmax.write_data = False
-            run_agents_on_mdp([lrmax], mdp1, n_instances=1, n_episodes=n_episodes, n_steps=n_steps,
-                              reset_at_terminal=False, verbose=False)
+            for job in jobs:
+                job.get()
+        else:
+            for i in trange(n_instances, desc='{:>10}'.format('instances')):
+                for j in trange(len(priors), desc='{:>10}'.format('priors')):
+                    run_twice(i, j, rmax, lrmax, priors[j], mdp1, mdp2, n_episodes, n_steps)
 
-            lrmax.write_data = True
-            run_agents_on_mdp([lrmax, rmax], mdp2, n_instances=1, n_episodes=n_episodes, n_steps=n_steps,
-                              reset_at_terminal=False, verbose=False)
-
-            # Retrieve data
-            df_lrmax = lrmax.data
-            df_rmax = rmax.data
-
-        instance_result = []
-        for i, _ in df_lrmax.iterrows():
-            prior = df_lrmax['prior'][i]
-            ratio_lip_bound_use = 100. * df_lrmax['ratio_lip_bound_use'][i]
-            speed_up = 100. * (df_rmax['cnt_time_steps_cv'][i] - df_lrmax['cnt_time_steps_cv'][i]) / df_rmax['cnt_time_steps_cv'][i]
-            instance_result.append([prior, ratio_lip_bound_use, speed_up])
-        results.append(instance_result)
-    print()
-
-    # Gather results
-    csv_write(['prior', 'ratio_lip_bound_use_mean', 'ratio_lip_bound_use_upper', 'ratio_lip_bound_use_lower',
-               'speed_up_mean', 'speed_up_upper', 'speed_up_lower'], save_path, 'w')
-    for i in range(len(priors)):
-        rlbu = []
-        su = []
-        for result in results:
-            rlbu.append(result[i][1])
-            su.append(result[i][2])
-        rlbu_mci = mean_confidence_interval(rlbu, confidence=.9)
-        su_mci = mean_confidence_interval(su, confidence=.9)
-        csv_write([priors[i]] + list(rlbu_mci) + list(su_mci), save_path, 'a')
-    if verbose:
-        for result in results:
-            print(result)
-
-    if plot:
-        plot_bound_use(save_path)
+            '''
+            instance_result = []
+            for i, _ in df_lrmax.iterrows():
+                prior = df_lrmax['prior'][i]
+                ratio_lip_bound_use = 100. * df_lrmax['ratio_lip_bound_use'][i]
+                speed_up = 100. * (df_rmax['cnt_time_steps_cv'][i] - df_lrmax['cnt_time_steps_cv'][i]) / df_rmax['cnt_time_steps_cv'][i]
+                instance_result.append([prior, ratio_lip_bound_use, speed_up])
+            results.append(instance_result)
+            '''
+        # Gather results
+        '''
+        csv_write(['prior', 'ratio_lip_bound_use_mean', 'ratio_lip_bound_use_upper', 'ratio_lip_bound_use_lower',
+                   'speed_up_mean', 'speed_up_upper', 'speed_up_lower'], save_path, 'w')
+        for i in range(len(priors)):
+            rlbu = []
+            su = []
+            for result in results:
+                rlbu.append(result[i][1])
+                su.append(result[i][2])
+            rlbu_mci = mean_confidence_interval(rlbu, confidence=.9)
+            su_mci = mean_confidence_interval(su, confidence=.9)
+            csv_write([priors[i]] + list(rlbu_mci) + list(su_mci), save_path, 'a')
+        if verbose:
+            for result in results:
+                print(result)
+        '''
+    if do_plot:
+        plot_bound_use(lrmax_path, rmax_path, len(priors), open_plot)
 
 
 if __name__ == '__main__':
-    bounds_comparison_experiment(plot=True)
+    bounds_comparison_experiment(do_run=False, do_plot=True, open_plot=True)
     # plot_bound_use('results/bounds-use-and-speed-up/gridworld_h-2_w-2/data.csv')
